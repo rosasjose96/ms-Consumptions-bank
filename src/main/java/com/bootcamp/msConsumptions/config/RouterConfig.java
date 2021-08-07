6f059d8c072e215b22a7618180614df0066baf4a
Jarg96Everis <88355059+Jarg96Everis@users.noreply.github.com>
2021-08-06 20:31:51 -0500

    Feat add CRUD handler complete

--- src/main/java/com/bootcamp/msConsumptions/config/RouterConfig.java
+++ src/main/java/com/bootcamp/msConsumptions/config/RouterConfig.java
@@ -1,15 +1,14 @@
 package com.bootcamp.msConsumptions.config;
 
-import com.bootcamp.msConsumptions.entities.CreditCardDTO;
 import com.bootcamp.msConsumptions.handler.ConsumptionCreditCardHandler;
 import com.bootcamp.msConsumptions.handler.ConsumptionHandler;
 import com.bootcamp.msConsumptions.handler.CreditCardDTOHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -18,22 +17,22 @@ public class RouterConfig {
     @Bean
     public RouterFunction<ServerResponse> routes(ConsumptionHandler consumptionHandler,
                                                  CreditCardDTOHandler creditCardDTOHandler,
-                                                 ConsumptionCreditCardHandler conssumptionCreditCardHandler){
+                                                 ConsumptionCreditCardHandler consumptionCreditCardHandler){
 
-        return route(RequestPredicates.GET("/api/consumption"), consumptionHandler::findAll)
-                .andRoute(RequestPredicates.GET("/api/consumption/{id}"), consumptionHandler::findConsumption)
-                .andRoute(RequestPredicates.POST("/api/consumption"), consumptionHandler::newConsumption)
-                .andRoute(RequestPredicates.PUT("/api/consumption/{id}"), consumptionHandler::updateConsumption)
-                .andRoute(RequestPredicates.DELETE("/api/consumption/{id}"), consumptionHandler::deleteConsumption)
-                .andRoute(RequestPredicates.GET("/api/consumptioncreditcarddto"), creditCardDTOHandler::findAll)
-                .andRoute(RequestPredicates.GET("/api/consumptioncreditcarddto/{id}"), creditCardDTOHandler::findCreditCardDTO)
-                .andRoute(RequestPredicates.POST("/api/consumptioncreditcarddto"), creditCardDTOHandler::newCreditCardDTO)
-                .andRoute(RequestPredicates.PUT("/api/consumptioncreditcarddto/{id}"), creditCardDTOHandler::updateCreditCardDTO)
-                .andRoute(RequestPredicates.DELETE("/api/consumptioncreditcarddto/{id}"), creditCardDTOHandler::deleteCreditCardDTO)
-                .andRoute(RequestPredicates.GET("/api/consumptioncreditcarddto"), conssumptionCreditCardHandler::findAll)
-                .andRoute(RequestPredicates.GET("/api/consumptioncreditcarddto/{id}"), conssumptionCreditCardHandler::findConsumptionCreditCard)
-                .andRoute(RequestPredicates.POST("/api/consumptioncreditcarddto"), conssumptionCreditCardHandler::newConsumptionCreditCard)
-                .andRoute(RequestPredicates.PUT("/api/consumptioncreditcarddto/{id}"), conssumptionCreditCardHandler::updateConsumptionCreditCard)
-                .andRoute(RequestPredicates.DELETE("/api/consumptioncreditcarddto/{id}"), conssumptionCreditCardHandler::deleteConsumptionCreditCard);
+        return route(GET("/api/consumption"), consumptionHandler::findAll)
+                .andRoute(GET("/api/consumption/{id}"), consumptionHandler::findConsumption)
+                .andRoute(POST("/api/consumption"), consumptionHandler::newConsumption)
+                .andRoute(PUT("/api/consumption/{id}"), consumptionHandler::updateConsumption)
+                .andRoute(DELETE("/api/consumption/{id}"), consumptionHandler::deleteConsumption)
+                .andRoute(GET("/api/creditcarddto"), creditCardDTOHandler::findAll)
+                .andRoute(GET("/api/creditcarddto/{id}"), creditCardDTOHandler::findCreditCardDTO)
+                .andRoute(POST("/api/creditcarddto"), creditCardDTOHandler::newCreditCardDTO)
+                .andRoute(PUT("/api/creditcarddto/{id}"), creditCardDTOHandler::updateCreditCardDTO)
+                .andRoute(DELETE("/api/creditcarddto/{id}"), creditCardDTOHandler::deleteCreditCardDTO)
+                .andRoute(GET("/api/consumptioncreditcar"), consumptionCreditCardHandler::findAll)
+                .andRoute(GET("/api/consumptioncreditcar/{id}"), consumptionCreditCardHandler::findConsumptionCreditCard)
+                .andRoute(POST("/api/consumptioncreditcar"), consumptionCreditCardHandler::newConsumptionCreditCard)
+                .andRoute(PUT("/api/consumptioncreditcar/{id}"), consumptionCreditCardHandler::updateConsumptionCreditCard)
+                .andRoute(DELETE("/api/consumptioncreditcar/{id}"), consumptionCreditCardHandler::deleteConsumptionCreditCard);
     }
 }
